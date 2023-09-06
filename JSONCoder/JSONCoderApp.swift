b2d3b304e07bc517bf4900283ee01d90047eaf08
肖扬 <xiaoyang.coffee@bytedance.com>
2023-09-06 23:54:58 +0800

    Initial Commit

---
+++ JSONCoder/JSONCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONCoderApp.swift
+//  JSONCoder
+//
+//  Created by ByteDance on 2023/9/6.
+//
+
+import SwiftUI
+
+@main
+struct JSONCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
